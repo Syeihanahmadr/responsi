5a6c4f4f9f1d6bc49a52be7d4e2381d5ee4d7925
Syeihanahmadr <syeihanar@students.amikom.ac.id>
2021-04-13 15:04:14 +0700

    tugas responsi

---
+++ Karyawan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman3368
+{
+    class Karyawan
+    {
+        private int nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public int Nik
+        {
+            get { return nik; }
+            set { nik = value; }
+        }
+        public int GajiBulanan
+        {
+            get { return gajiBulanan; }
+            set { gajiBulanan = value; }
+        }
+
+        public Karyawan(string nama, int nik, int gajiBulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            if (gajiBulanan <= 0)
+            {
+                GajiBulanan = 0;
+            }else
+            {
+                GajiBulanan = gajiBulanan;
+            }
+           
+        }
+
+        
+        public void Menampilkan(int no, int nik, string nama, int gajiBulanan)
+        {
+         
+           // Console.WriteLine("{0}. {1} {2}\t({3})",no,nik,nama,gajiBulanan);
+            Console.WriteLine(no + ". " + nik +" " + nama +"\t" + String.Format("{0:n0}",gajiBulanan));
+        }
+        
+        public int PeningkatanGaji(int gajiBulanan)
+        {
+            GajiBulanan = gajiBulanan + ((gajiBulanan / 100) * 10);
+
+            return 0;
+        }
+
+    }
+}
---
+++ Program (1).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman3368
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("Paijo", 190302123, 3000000);
+            Karyawan karyawan2 = new Karyawan("Jono", 190302124, 2000000);
+
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------");
+
+            karyawan1.Menampilkan(1, karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            karyawan2.Menampilkan(2, karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Alhamdulillah dapat kenaikan gaji 10%");
+            Console.WriteLine();
+
+            
+            karyawan1.PeningkatanGaji(karyawan1.GajiBulanan);
+            karyawan2.PeningkatanGaji(karyawan2.GajiBulanan);
+
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("------------------------------------");
+            karyawan1.Menampilkan(1, karyawan1.Nik, karyawan1.Nama, karyawan1.GajiBulanan);
+            karyawan2.Menampilkan(2, karyawan2.Nik, karyawan2.Nama, karyawan2.GajiBulanan);
+
+
+            Console.ReadKey();
+        }
+    }
+}
